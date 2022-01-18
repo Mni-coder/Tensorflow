7157a2392cf62e2cbf48039dd2caeb0f217bbf3e
Mni-coder <80754506+Mni-coder@users.noreply.github.com>
2022-01-19 05:46:30 +0800

    Update backward.py

--- Practice in Open cource from Peking U/backward.py
+++ Practice in Open cource from Peking U/backward.py
@@ -5,9 +5,8 @@ import os
 
 BATCH_SIZE = 200
 LEARNING_RATE = 0.001
-REGULARIZER = 0.0001
 STEPS = 50000
-MOVING_AVERAGE_DECAY = 0.99
+
 MODEL_SAVE_PATH="./model/"
 MODEL_NAME="mnist_model"
 
@@ -15,7 +14,7 @@ MODEL_NAME="mnist_model"
 def backward(mnist):
     x = tf.placeholder(tf.float32, [None, mnist_forward.INPUT_NODE])
     y_ = tf.placeholder(tf.float32, [None, mnist_forward.OUTPUT_NODE])
-    y = mnist_forward.forward(x, REGULARIZER)
+    y = mnist_forward.forward(x)
     global_step = tf.Variable(0, trainable=False)
 
     loss = tf.reduce_mean(tf.square(y_-y))
@@ -31,7 +30,7 @@ def backward(mnist):
 
         for i in range(STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
-            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
+            _, loss_value, step = sess.run([train_step, loss, global_step], feed_dict={x: xs, y_: ys})
             if i % 1000 == 0:
                 print("After %d training step(s), loss on training batch is %g." % (step, loss_value))
                 saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
